df5efb7884754beda903198fd2e13aa13aa38d94
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-16 16:59:55 +0200

    fixed mistakes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ const listOfCountries = document.querySelector('.country-list');
 
 const onSearchInput = event => {
     const country = event.target.value.trim();
+    if (!country) {
+        listOfCountries.innerHTML = '';
+        countryCard.innerHTML = '';
+        return;
+    }
     fetchCountries(country).then(countries => {
         if (countries.length === 1) {
         renderCountryCard(countries);
@@ -23,11 +28,9 @@ const onSearchInput = event => {
     }
         
     }).catch(error => {
-        if (error.message === '404') {
             Notify.failure('Oops, there is no country with that name');
             listOfCountries.innerHTML = '';
             countryCard.innerHTML = '';
-        }
     })
 }
 
@@ -37,7 +40,7 @@ textInput.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 function renderCountryCard(countries) {
     const markup = countries.map(country => {
     return `<div> 
-    <img src = '${country.flags.svg}' alt = 'flag' width = '50'>
+    <img src = '${country.flags.svg}' alt = 'flag' width = '80'>
     <h1>${country.name.official}</h1>
     <p class='text'>Capital: ${country.capital}</p>
     <p class='text'>Population: ${country.population}</p>
@@ -51,7 +54,7 @@ listOfCountries.innerHTML = '';
 function renderListOfCountries(countries) {
     const listMarkup = countries.map(country => {
         return `<li style = 'list-style: none; display: flex; gap: 10px'>
-        <img src = '${country.flags.svg}' alt = 'flag' width = '50'>
+        <img src = '${country.flags.svg}' alt = 'flag' width = '80' height = '40'>
         <p>${country.name.official}</p>
         </li>`
     }).join('');
